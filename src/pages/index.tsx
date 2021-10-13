928116bef96f5f74de750517d9fd4f7937544ab0
Igor Souza <71149968+Igorsouza1@users.noreply.github.com>
2021-10-13 16:29:24 -0400

    finish config

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import  Head  from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>New Title</title>
+    </Head>
+
+    <h1 >  
+      Hello World
+    </h1>
+    </> 
+  )
+}
