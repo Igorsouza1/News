1762e3fb8d1b88d6255614aa0f82bc45109b57bb
Igor Souza <71149968+Igorsouza1@users.noreply.github.com>
2021-10-14 15:01:36 -0400

    Home page 90%, faltando botao subscriber

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import  Head  from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
     <Head>
-        <title>New Title</title>
+        <title>Home</title>
     </Head>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, Welcome</span>
+        <h1>News about the <span>React</span> World.</h1>
+        <p>
+          Get acess to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
 
-    <h1 >  
-      Hello World
-    </h1>
+      <img src="/images/avatar.svg" alt="girl coding" />
+    </main>
     </> 
   )
 }
